47d926b1d4533aee98ba80d6677b78af7156dbf5
Wink Saville <wink@saville.com>
2017-04-30 10:43:20 -0700

    Rename traceloc.spec.es6.js to traceloc.spec.js

--- src/traceloc.spec.js
+++ src/traceloc.spec.js
@@ -1,14 +1,12 @@
 const Expect = require("alsatian").Expect;
-// const @Test = require("alsatian").@Test;
-const TraceLoc = require("../out/traceloc").TraceLoc;
 const here = require("../out/traceloc").here;
 
 // A self executing anonymous function
 (function() {
     console.log("anonymous function");
-    let loc = new TraceLoc();
+    let loc = here();
     Expect(loc.func).not.toBeTruthy();
-    Expect(loc.file).toBe("src/traceloc.spec.es6.js");
+    Expect(loc.file).toBe("src/traceloc.spec.js");
     Expect(loc.line).toBeGreaterThan(0);
     Expect(loc.col).toBeGreaterThan(0);
 })();
@@ -17,7 +15,7 @@ function test() {
     console.log("test");
     let loc = here();
     Expect(loc.func).toBe("test");
-    Expect(loc.file).toBe("src/traceloc.spec.es6.js");
+    Expect(loc.file).toBe("src/traceloc.spec.js");
     Expect(loc.line).toBeGreaterThan(0);
     Expect(loc.col).toBeGreaterThan(0);
 }
