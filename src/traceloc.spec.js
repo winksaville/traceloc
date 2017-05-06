b2fee3e1932ab4ba59c91efd2a2e706448541f78
Wink Saville <wink@saville.com>
2017-05-06 08:49:40 -0700

    Don't use __dirname with setProjectRoot
    This is node js specific and something like "." or "./src" is better.

--- src/traceloc.spec.js
+++ src/traceloc.spec.js
@@ -2,14 +2,14 @@ const Expect = require("alsatian").Expect;
 const here = require("../out/traceloc").here;
 const setProjectRoot = require("../out/traceloc").setProjectRoot;
 
-setProjectRoot(__dirname);
+setProjectRoot("./src");
 
 // A self executing anonymous function
 (function() {
     console.log("anonymous function");
     let loc = here();
     Expect(loc.func).not.toBeTruthy();
-    Expect(loc.file).toBe("src/traceloc.spec.js");
+    Expect(loc.file).toBe("traceloc.spec.js");
     Expect(loc.line).toBeGreaterThan(0);
     Expect(loc.col).toBeGreaterThan(0);
 })();
@@ -18,7 +18,7 @@ function test() {
     console.log("test");
     let loc = here();
     Expect(loc.func).toBe("test");
-    Expect(loc.file).toBe("src/traceloc.spec.js");
+    Expect(loc.file).toBe("traceloc.spec.js");
     Expect(loc.line).toBeGreaterThan(0);
     Expect(loc.col).toBeGreaterThan(0);
 }
